4b090980c92ef068ad27201d3e2b3641a5a76564
kkyeonjin <r435675@gmail.com>
2022-03-10 21:25:21 +0900

    Update 12840.cpp

--- assignment/12840.cpp
+++ assignment/12840.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int h, m, s, q, t, c;
     int time_current;
     cin >> h >> m >> s;
@@ -14,19 +16,19 @@ int main(){
     time_current = h*3600 + m*60 + s;
     for(int i=0; i<q; i++){
         cin >> t;
-        if(t == 1 ) {
+        if(t == 3 ) {
+            cout << time_current/3600 << " " << (time_current/60)%60 << " " << time_current%60 << "\n";
+        }
+        else if(t == 1) {
             cin >> c;
             time_current = (time_current+c)%86400;
         }
-        else if(t == 2) {
+        else{
             cin >> c;
-            time_current = (time_current-c)%86400;
-            if(time_current < 0){
+            time_current = (time_current - c) %86400;
+            while(time_current < 0) {
                 time_current += 86400;
             }
-        }
-        else{
-            cout << time_current/3600 << " " << (time_current%3600)/60 << " " << time_current%60 ;
         }
 
     }
